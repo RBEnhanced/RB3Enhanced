5f72dfae4890563bed4f444504f6b9ea29726857
jnack <55568980+jnackmclain@users.noreply.github.com>
2025-05-24 18:51:45 -0500

    oh remove this?

--- source/SetlistHooks.c
+++ source/SetlistHooks.c
@@ -23,80 +23,6 @@
 #include "rb3/Rnd/RndMat.h"
 
 DynamicTex *textures[100] = {0};
-static char *originToIcon[][2] = {
-    {"rb1", "<alt>Y</alt> "},
-    {"rb2", "<alt>2</alt> "},
-    {"rb3", "<alt>B</alt> "},
-    {"rb4", "<alt>1</alt> "},
-    {"rb_blitz", "<alt>X</alt> "},
-    {"ugc", "<alt>U</alt> "},
-    {"ugc_c3", "<alt>y</alt> "},
-    {"ugc_plus", "<alt>U</alt> "},
-    {"ugc1", "<alt>U</alt> "},
-    {"ugc2", "<alt>U</alt> "},
-    {"lego", "<alt>A</alt> "},
-    {"greenday", "<alt>0</alt> "},
-    {"beatles", "<alt>b</alt> "},
-    {"gh1", "<alt>R</alt> "},
-    {"gh2", "<alt>S</alt> "},
-    {"gh3", "<alt>s</alt> "},
-    {"onyxite", "<alt>G</alt> "},
-};
-static int numOriginToIcon = sizeof(originToIcon) / sizeof(originToIcon[0]);
-
-void SetSongAndArtistNameHook(BandLabel *label, SortNode *sortNode)
-{
-    char newLabel[1024] = {0};
-    char *originLabel = "<alt>0</alt> "; // default
-    int i = 0;
-
-    if (config.GameOriginIcons == 1 && strlen(label->string) < 1000)
-    {
-        SetSongAndArtistName(label, sortNode);
-        for (i = 0; i < numOriginToIcon; i++)
-        {
-            if (strcmp(_Unknown2->metaData->gameOrigin.sym, originToIcon[i][0]) == 0)
-            {
-                originLabel = originToIcon[i][1];
-                break;
-            }
-        }
-        strcat(newLabel, originLabel);
-        strcat(newLabel, label->string);
-        BandLabelSetDisplayText(label, newLabel, 1);
-        return;
-    }
-    SetSongAndArtistName(label, sortNode);
-    return;
-}
-
-void SetSongNameFromNodeHook(BandLabel *label, SortNode *sortNode)
-{
-    char newLabel[1024] = {0};
-    char *originLabel = "<alt>0</alt> "; // default
-    int i = 0;
-
-    RB3E_DEBUG("SetSongNameFromNode: %s", label->string);
-
-    if (config.GameOriginIcons == 1 && strlen(label->string) < 1000)
-    {
-        SetSongNameFromNode(label, sortNode);
-        for (i = 0; i < numOriginToIcon; i++)
-        {
-            if (strcmp(_Unknown2->metaData->gameOrigin.sym, originToIcon[i][0]) == 0)
-            {
-                originLabel = originToIcon[i][1];
-                break;
-            }
-        }
-        strcat(newLabel, originLabel);
-        strcat(newLabel, label->string);
-        BandLabelSetDisplayText(label, newLabel, 1);
-        return;
-    }
-    SetSongNameFromNode(label, sortNode);
-    return;
-}
 
 void CreateMaterial(GameOriginInfo *info)
 {
@@ -156,7 +82,7 @@ RndMat *MusicLibraryMatHook(int *thisMusicLibrary, int data, int idx, UIListSlot
             ret = SongSortMgrGetSort(*(SongSortMgr **)PORT_THESONGSORTMGR, thisMusicLibrary->mSortType);
             if (ret != NULL)
             {
-                node = NodeSortGetNode(int, idx);
+                node = NodeSortGetNode(ret, idx);
                 if (node != NULL)
                 {
                     nodeType = node->vtable->getNodeType();
@@ -194,7 +120,7 @@ RndMat *MusicLibraryMatHook(int *thisMusicLibrary, int data, int idx, UIListSlot
         }
     }
 
-    return MusicLibraryMat(thisMusicLibrary, data, idx, UIListSlot);
+    return MusicLibraryMat(thisMusicLibrary, data, idx, listSlot);
 }
 
 int numGameOrigins;
