5e6b1ab740c783e7ad4b4aa7d741df603657212a
jnack <55568980+jnackmclain@users.noreply.github.com>
2026-08-09 01:10:10 -0500

    Add "-debug" notice to build tag in debug builds (#61)

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,3 +1,8 @@
+if [ "$1" = "DEBUG" ]; then
+    SUFF="-debug"
+else
+    SUFF=""
+fi
 if git --version &>/dev/null; then
 # if git is installed, get the current information from the repository
     [[ ! -z ${GITHUB_ACTIONS} ]] && [[ ${RUNNER_OS} == "Linux" ]] && git config --global --add safe.directory /__w/RB3Enhanced/RB3Enhanced
@@ -5,9 +10,9 @@ if git --version &>/dev/null; then
 // This file will be auto generated during building, and should
 // not be modified manually
 // The git tag used to compile RB3Enhanced
-#define RB3E_BUILDTAG "$(git describe --always --tags --dirty)"
+#define RB3E_BUILDTAG "$(git describe --always --tags --dirty)$SUFF"
 // The git branch and commit used to compile RB3Enhanced
-#define RB3E_BUILDCOMMIT "$(git rev-parse --abbrev-ref HEAD)-$(git describe --always --match \"tag\" --dirty)"
+#define RB3E_BUILDCOMMIT "$(git rev-parse --abbrev-ref HEAD)-$(git describe --always --match \"tag\" --dirty)$SUFF"
 EOF
 else
 # if git isn't installed, we don't know the version, use the compile time in logs
@@ -15,8 +20,8 @@ else
 // This file will be auto generated during building, and should
 // not be modified manually
 // The git tag used to compile RB3Enhanced
-#define RB3E_BUILDTAG "Unknown"
+#define RB3E_BUILDTAG "Unknown$SUFF"
 // The git branch and commit used to compile RB3Enhanced
-#define RB3E_BUILDCOMMIT "$(date) @ $HOSTNAME"
+#define RB3E_BUILDCOMMIT "$(date) @ $HOSTNAME$SUFF"
 EOF
 fi
